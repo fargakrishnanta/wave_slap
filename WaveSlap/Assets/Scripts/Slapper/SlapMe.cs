7dc7439ec706e2c6bfed09e541be2a5e9c3c6d58
Farga Krishnanta <f_krishnanta@live.com>
2017-01-21 00:23:18 -0800

    Added Slapping ability for Slappy

--- WaveSlap/Assets/Scripts/Slapper/SlapMe.cs
+++ WaveSlap/Assets/Scripts/Slapper/SlapMe.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class SlapMe : MonoBehaviour {
 
+    [SerializeField]
+    public float Boundary;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,43 @@ public class SlapMe : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetMouseButtonDown(0))
+        {
+            SummonDaRay();
+        }
 	}
+
+    /*Ray Cast dem slap mouse click*/
+    void SummonDaRay()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.Log(ray.origin.ToString());
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Person"))
+            {
+                if (CheckInBoundary(hit.collider.gameObject))
+                {
+                    SlapMePlease();
+                }
+            }
+
+        }
+    }
+
+    /*Check if you are within the range to slap*/
+    bool CheckInBoundary(GameObject go)
+    {
+        if ((this.transform.position - go.transform.position).magnitude <= Boundary)
+            return true;
+
+        return false;
+    }
+
+    //TODO Slapping
+    void SlapMePlease()
+    {
+        Debug.Log("SLAPPPPP ME");
+    }
 }
