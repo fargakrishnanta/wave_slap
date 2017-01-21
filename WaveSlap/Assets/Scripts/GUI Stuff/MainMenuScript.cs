9749488d04969b3b1e7f2b4cde2f070944c54b4a
Farga Krishnanta <f_krishnanta@live.com>
2017-01-21 01:42:21 -0800

    Added some logic for mainmenu

---
+++ WaveSlap/Assets/Scripts/GUI Stuff/MainMenuScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+
+    public GameObject Waver;
+    public GameObject Slapper;
+    public GameObject MiddlePoint;
+    public GameObject StartPanel;
+    public GameObject WaSlapPanel;
+
+    private Rigidbody2D m_WaverRB;
+    private Rigidbody2D m_SlapperRB;
+	// Use this for initialization
+	void Start () {
+        m_SlapperRB = Slapper.GetComponent<Rigidbody2D>();
+        m_WaverRB = Waver.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (HighFive())
+            StartPanel.SetActive(true);
+	}
+
+    bool HighFive()
+    {
+        float m_step = 2 * Time.fixedDeltaTime;
+        Slapper.transform.position = Vector2.MoveTowards(Slapper.transform.position, MiddlePoint.transform.position, m_step);
+        Waver.transform.position = Vector2.MoveTowards(Waver.transform.position, MiddlePoint.transform.position, m_step);
+        if((Slapper.transform.position == MiddlePoint.transform.position) && (Waver.transform.position == MiddlePoint.transform.position))
+        {
+            WaSlapPanel.SetActive(false);
+            return true;
+        }
+
+        return false;
+    }
+}
