e59b6184e15c3cabcc19f047599c1fc7e6aee246
Farga Krishnanta <f_krishnanta@live.com>
2017-01-21 04:35:54 -0800

    Added custome sprite for timer TIME TO SLEEP

---
+++ WaveSlap/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class CountdownTimer : MonoBehaviour {
+
+    [SerializeField]
+    public float MaxTime;
+
+    public Sprite S1;
+    public Sprite S2;
+    public Sprite S3;
+    public Sprite S4;
+    public Sprite S5;
+    public Sprite S6;
+    public Sprite S7;
+    public Sprite S8;
+    public Sprite S9;
+    public Sprite S0;
+
+
+    public Image MOne;
+    public Image MTwo;
+    public Image SOne;
+    public Image STwo;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MaxTime -= Time.deltaTime;
+        PrintTime();
+	}
+
+    void PrintTime()
+    {
+        string minutes = Mathf.Floor(MaxTime / 60).ToString("00");
+        string seconds = Mathf.Floor(MaxTime % 60).ToString("00");
+
+        string whatever = minutes + ":" + seconds;
+
+        int index = 0;
+        foreach(var letter in minutes)
+        {
+            if (index == 2)
+                index = 0;
+            if (index == 0)
+            {
+                MOne.sprite = SpriteSelection(letter.ToString());
+            }
+            else if(index == 1)
+            {
+                MTwo.sprite = SpriteSelection(letter.ToString());
+            }
+            index++;
+           
+        }
+        index = 0;
+        foreach (var letter in seconds)
+        {
+            if (index == 2)
+                index = 0;
+            if (index == 0)
+            {
+                SOne.sprite = SpriteSelection(letter.ToString());
+            }
+            else if (index == 1)
+            {
+                STwo.sprite = SpriteSelection(letter.ToString());
+            }
+            index++;
+
+        }
+
+    }
+
+    Sprite SpriteSelection(string a)
+    {
+        switch (a)
+        {
+            case "1":
+                return S1; 
+            case "2":
+                return S2;
+            case "3":
+                return S3;
+            case "4":
+                return S4;
+            case "5":
+                return S5;
+            case "6":
+                return S6;
+            case "7":
+                return S7;
+            case "8":
+                return S8;
+            case "9":
+                return S9;
+            case "0":
+                return S0;
+            default:
+                return S0;
+        }
+    }
+}
