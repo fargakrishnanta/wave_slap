944ef56e11287729d74694a600c6f10dfa4b697c
thunduaga <chriswumbojumbo@msn.com>
2017-01-21 17:22:05 -0800

    added waver back into prefabs.. changed max happiness in NPC prefabs back to 3 and their min to 1 and their current to 1 same with waver

--- WaveSlap/Assets/Scripts/HappyController.cs
+++ WaveSlap/Assets/Scripts/HappyController.cs
@@ -18,7 +18,7 @@ public class HappyController : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-
+        em = GameObject.Find("EventManager").GetComponent<EventManager>();
 	}
 	
 	// Update is called once per frame
