73514f7df9a2f098ea8d43817618ae3b07203a6b
LuckiiGoggy <luck-13@live.ca>
2017-01-22 14:11:32 -0800

    Win Scenes

--- WaveSlap/Assets/Scripts/GlobalHappyController.cs
+++ WaveSlap/Assets/Scripts/GlobalHappyController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class GlobalHappyController : MonoBehaviour {
 
@@ -431,7 +432,7 @@ public class GlobalHappyController : MonoBehaviour {
         }
 
         if(totalHappyScore >= maxHappinessLevels * numOfNPC) {
-            Debug.Log("Waver Wins");
+            SceneManager.LoadScene("WaverWinScene");
         }
 
         //update the 
