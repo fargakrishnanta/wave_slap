ada3251163cdd5ea4a4d6ce84b67c58a1617c4f7
Farga Krishnanta <f_krishnanta@live.com>
2017-01-22 12:08:50 -0800

    huehuehuehueuuehuhehehuehuejajajaj

---
+++ WaveSlap/Assets/Scripts/InstructionCutSceneScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionCutSceneScript : MonoBehaviour {
+
+    [SerializeField]
+    public float MaxTimer;
+
+    public GameManager gm;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MaxTimer -= Time.fixedDeltaTime;
+        if (MaxTimer <= 0)
+            gm.StartSlapperCutScene();
+	}
+}
