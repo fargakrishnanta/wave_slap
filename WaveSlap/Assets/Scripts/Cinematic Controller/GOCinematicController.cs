5c643d34a2dcbdd9f736cc2d8cbdcde19fd7c5cd
LuckiiGoggy <luck-13@live.ca>
2017-01-22 09:22:57 -0800

    Cinematic Controller

---
+++ WaveSlap/Assets/Scripts/Cinematic Controller/GOCinematicController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+
+public class GOCinematicController : MonoBehaviour {
+
+    public List<GOAnimation> animationList;
+    public List<GOAnimation> removeList;
+
+    public float delay;
+
+
+	// Use this for initialization
+	void Start () {
+        removeList = new List<GOAnimation>();
+
+
+        foreach (GOAnimation anim in animationList) {
+            anim.InitializeAnim();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    foreach(GOAnimation anim in animationList) {
+            anim.NextFrame(Time.deltaTime);
+
+            if (anim.isComplete) {
+                removeList.Add(anim);
+            }
+        }   
+
+        foreach(GOAnimation anim in removeList) {
+            animationList.Remove(anim);
+        }
+	}
+}
