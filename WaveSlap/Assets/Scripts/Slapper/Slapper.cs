fbd47faa991570b8cc4cd134c3f2371eb6149d5b
LuckiiGoggy <luck-13@live.ca>
2017-01-21 17:47:33 -0800

    Slapper Animation Added

--- WaveSlap/Assets/Scripts/Slapper/Slapper.cs
+++ WaveSlap/Assets/Scripts/Slapper/Slapper.cs
@@ -2,13 +2,16 @@
 
 using System.Collections;
 
-public class Slapper : MonoBehaviour {
+public class Slapper : MovementControl {
 
     private GameObject Slappy;
     private Rigidbody2D m_rb;
+    [SerializeField]
     private float m_speed = 1;
     private Vector3 m_target;
     private bool m_isSet;
+
+    private Animator animator;
 
 	// Use this for initialization
 	void Start () {
@@ -18,7 +21,8 @@ public class Slapper : MonoBehaviour {
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.lockState = CursorLockMode.None;
-     
+
+        animator = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -35,7 +39,19 @@ public class Slapper : MonoBehaviour {
         //set Z coord so the obj wont go up
         m_target.z = transform.position.z;
         //move it! :)
-        transform.position = Vector3.MoveTowards(transform.position, m_target, m_speed * Time.fixedDeltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, m_target, m_speed * Time.fixedDeltaTime);
 
+        Vector3 deltaVel = (m_target - transform.position);
+
+        m_rb.velocity = deltaVel.normalized * m_speed;
+
+        FlipIt(deltaVel);
+
+        if(m_rb.velocity.magnitude > 0.01f) {
+            animator.SetBool("Moving", true);
+        }
+        else {
+            animator.SetBool("Moving", false);
+        }
     }
 }
