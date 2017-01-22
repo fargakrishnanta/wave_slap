d5fad1a4adb2927ba0883175c9215b42f496a81d
LuckiiGoggy <luck-13@live.ca>
2017-01-21 19:15:52 -0800

    Suf

--- WaveSlap/Assets/Scripts/Slapper/Slapper.cs
+++ WaveSlap/Assets/Scripts/Slapper/Slapper.cs
@@ -39,15 +39,14 @@ public class Slapper : MovementControl {
         //set Z coord so the obj wont go up
         m_target.z = transform.position.z;
         //move it! :)
-        //transform.position = Vector3.MoveTowards(transform.position, m_target, m_speed * Time.fixedDeltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, m_target, m_speed * Time.fixedDeltaTime);
 
         Vector3 deltaVel = (m_target - transform.position);
 
-        m_rb.velocity = deltaVel.normalized * m_speed;
 
         FlipIt(deltaVel);
 
-        if(m_rb.velocity.magnitude > 0.01f) {
+        if(deltaVel.magnitude > 0.001f) {
             animator.SetBool("Moving", true);
         }
         else {
